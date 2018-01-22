b23fa95f2b24db3f15d3525743686e6e537e64d7
ss <ss@192.168.1.203>
2018-01-21 23:04:31 -0500

    ddsad

--- src/RatInaMaze.java
+++ src/RatInaMaze.java
@@ -1,13 +1,13 @@
 /*
- * Given a maze, NxN matrix. A rat has to find a path from source to des­ti­na­tion. maze[0][0] (left top corner)is the source and maze[N-1][N-1](right bot­tom cor­ner) is des­ti­na­tion.
- *  There are few cells which are blocked, means rat can­not enter into those cells. Rat can move in any direc­tion ( left, right, up and down).
+ * Given a maze, NxN matrix. A rat has to find a path from source to desï¿½tiï¿½naï¿½tion. maze[0][0] (left top corner)is the source and maze[N-1][N-1](right botï¿½tom corï¿½ner) is desï¿½tiï¿½naï¿½tion.
+ *  There are few cells which are blocked, means rat canï¿½not enter into those cells. Rat can move in any direcï¿½tion ( left, right, up and down).
 
-Input: A 2D-matrix with 0’s and 1’s fill in it. 0 means that cell is blocked and 1 means rat can move to that cell.
+Input: A 2D-matrix with 0ï¿½s and 1ï¿½s fill in it. 0 means that cell is blocked and 1 means rat can move to that cell.
  */
 public class RatInaMaze {
 	
 	public static boolean ratInMaze(int[][] maze, int[][] sol, int i, int j, int N) {
-		if(i == N-1 && j == N-1) {
+		if(i == N-1 && j == N-1 && maze[i][j]==1) {
 			sol[i][j] = 1;
 			return true;
 		}
@@ -28,7 +28,7 @@ public class RatInaMaze {
 		int[][] maze = { {1, 0, 0, 0},
 						 {1, 1, 0, 1},
 						 {0, 1, 0, 0},
-						 {1, 1, 1, 0}}; 
+						 {1, 1, 1, 1}}; 
 		/*int[][] maze = { {1, 0, 1, 1, 1},
 				 {1, 1, 1, 0, 1},
 				 {0, 0, 0, 1, 1},
